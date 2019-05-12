3178e871ce0be464c8499433c3775b0410358177
franciscolagos <f.bunout01@ufromail.cl>
2019-05-12 00:19:15 -0400

    creation of img with only predominant color of the pixel

--- img_procesing/process.py
+++ img_procesing/process.py
@@ -9,4 +9,19 @@ print(pix[1,1]) #obtiene los balores en rgb del pixel de la imagen
 img.save('alive_parrot.png')
 for y in range(0, img.size[1]):
     for x in range(0, img.size[0]):
-        print(pix[x,y])
+        if pix[x,y][0]==100 and pix[x,y][1] ==100 and pix[x,y][2]==100:
+            pix[x, y] = (100, 100, 100)
+        else:
+            if pix[x,y][0]==0 and pix[x,y][1] ==0 and pix[x,y][2]==0:
+                pix[x, y] = (0, 0, 0)
+            else:
+                if pix[x, y][0] > pix[x, y][1]:
+                    if pix[x, y][0] > pix[x, y][2]:
+                        pix[x, y] = (100, 0, 0)
+                else:
+                    if pix[x, y][1] > pix[x, y][2]:
+                        pix[x, y] = (0, 100, 0)
+                    else:
+                        pix[x, y] = (0, 0, 100)
+
+img.save('alive_parrot.png')
