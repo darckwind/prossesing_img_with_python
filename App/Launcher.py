2ec4852f5f5951435b7662da215bcc620e336d1b
franciscolagos <f.bunout01@ufromail.cl>
2019-06-09 13:56:03 -0400

    Re-estruccturacion de carpetas, adicion de lib

---
+++ App/Launcher.py
@@ -0,0 +1,8 @@
+import Satelital_api.test as sat
+
+import img_procesing.process as img
+
+img
+
+sat.Satelite.descarga()
+
--- Satelital_api/test.py
+++ Satelital_api/test.py
@@ -1,24 +1,31 @@
 import landsatxplore.api
 import urllib.request
 
-api = landsatxplore.api.API("f.bunout01@ufromail.cl", "Pokemon22300")
+class Satelite:
 
-# Request
-scenes = api.search(
-    dataset='LANDSAT_8_C1',
-    latitude=-38.680782,
-    longitude=-71.2800889,
-    start_date='2019-01-24',
-    end_date='2019-02-01',
-    max_cloud_cover=1)
+    #metodo encargado de la descarga desde el satelite LANDSAT_8_C1
+    @staticmethod
+    def descarga():
+        api = landsatxplore.api.API("f.bunout01@ufromail.cl", "Pokemon22300")
 
-print('{} scenes found.'.format(len(scenes)))
+        # Request
+        scenes = api.search(
+            dataset='LANDSAT_8_C1',
+            latitude=-38.680782,
+            longitude=-71.2800889,
+            start_date='2019-01-24',
+            end_date='2019-02-01',
+            max_cloud_cover=1)
 
-print(scenes)
+        print('{} scenes found.'.format(len(scenes)))
 
-for scene in scenes:
-    print(scene['displayId'])
-    url = 'https://earthexplorer.usgs.gov/browse/full/landsat_8/'+scene['displayId']
-    print(url)
-    urllib.request.urlretrieve(url, '/Users/franciscolagos/PycharmProjects/untitled/satelital_img/satelital_org_img.jpeg')
-api.logout()
+        print(scenes)
+
+        for scene in scenes:
+            #print(scene['displayId'])
+            url = 'https://earthexplorer.usgs.gov/browse/full/landsat_8/' + scene['displayId']
+            #print(url)
+            urllib.request.urlretrieve(url,'/Users/franciscolagos/PycharmProjects/untitled/satelital_img/satelital_org_img.jpeg')
+        api.logout()
+
+Satelite.descarga()
